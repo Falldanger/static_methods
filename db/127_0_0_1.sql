016058c7ef37770c06f70e4fc4964f71281346db
Falldanger <bohdan.murga@yandex.ua>
2019-08-30 22:24:23 +0300

    completed a test task using static methods

---
+++ db/127_0_0_1.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.7
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Generation Time: Aug 30, 2019 at 10:16 PM
+-- Server version: 5.7.20
+-- PHP Version: 7.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `date_test`
+--
+CREATE DATABASE IF NOT EXISTS `date_test` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `date_test`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `test`
+--
+
+CREATE TABLE `test` (
+  `id` int(10) NOT NULL,
+  `date` timestamp NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `test`
+--
+
+INSERT INTO `test` (`id`, `date`) VALUES
+(1, '2019-08-30 02:19:15');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `test`
+--
+ALTER TABLE `test`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `test`
+--
+ALTER TABLE `test`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ index.php
@@ -0,0 +1,76 @@
+<?php
+	define('DB_HOST','localhost');
+    define('DB_USER', 'root');
+    define('DB_PASSWORD', '');
+    define('DB_NAME', 'date_test');
+
+    try{
+    $pdo= new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME,DB_USER,DB_PASSWORD,[PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION]);
+	}
+	catch(PDOException $e){
+		echo "Connection error";
+	}
+	
+	class Test{
+
+		public static $pos;
+		public static $lastWord;
+		public static $matches;
+		public static $date;
+		public static $getstr;
+
+		public static function last_word($sentence){
+			// position of last occuring space 
+			// in the string 
+			self::$pos = strrpos($sentence, ' '); 
+			  
+			// if the string has only one word 
+			if(!self::$pos) 
+			{ 
+			    self::$pos = 0; 
+			} 
+			else
+			{ 
+			    self::$pos = self::$pos + 1; 
+			} 
+			  
+			// get the last word in the string 
+			self::$lastWord = substr($sentence,self::$pos); 
+			  
+			// return length of last word  
+			return strlen(self::$lastWord); 
+		}
+
+		public static function sql_date_format($dateStr){
+			return self::$date=date("d-m-Y H:i:s",strtotime($dateStr));
+		}
+
+		public static function extract_string($str){
+			preg_match_all('#\[(.*?)\]#', $str, self::$matches);
+			self::$getstr='';
+			for ($i=0; $i <count(self::$matches) ; $i++) { 
+			self::$getstr=self::$getstr.' '.self::$matches[1][$i];
+			}
+			
+			return self::$getstr;
+		}
+	}
+
+echo '<b>Test task:</b></br>';
+
+//last_word function
+echo Test::last_word('Hello World').'</br>';
+echo Test::last_word('').'</br>';
+
+//sql_date_format function
+$query='SELECT `date` FROM `test`';
+	$result=$pdo->query($query);
+	$rows=$result->fetch();
+	$getdate=$rows['date'];
+	
+echo Test::sql_date_format($getdate).'</br>';
+
+//extract_string function
+echo Test::extract_string('quikly [brown fox] and little[catty] here').'</br>';
+
+?>
